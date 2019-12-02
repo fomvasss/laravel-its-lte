848cd73405f21ddd76426dd8d019b7e2c50e541e
its-001 <itspace.alpha@gmail.com>
2019-12-02 18:14:21 +0200

    upd user

--- resources/views/content/users/create.blade.php
+++ resources/views/content/users/create.blade.php
@@ -4,25 +4,29 @@
     $content_header = [
         'page_title' => 'Пользователи',
         'url_back' => session('users.index'),
-        'urlCreate' => ''
+        'url_create' => ''
     ]
 @endphp
 
 @section('content')
 <section class="content">
-    <div class="box">
-        {!! Form::open([
-             'url' => 'users.store',
-             'files' => true
-        ]) !!}
-        <div class="box-header">
-            <i class="ion ion-clipboard"></i>
-            <h3 class="box-title">Создание пользователя</h3>
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+            <div class="box">
+                {!! Form::open([
+                     'url' => 'users.store',
+                     'files' => true
+                ]) !!}
+                <div class="box-header">
+                    <i class="ion ion-clipboard"></i>
+                    <h3 class="box-title">Создание пользователя</h3>
+                </div>
+                <div class="box-body">
+                    @include('lte::content.users._form')
+                </div>
+                {!! Form::close() !!}
+            </div>
         </div>
-        <div class="box-body">
-            @include('lte::content.users._form')
-        </div>
-        {!! Form::close() !!}
     </div>
 </section>
 @stop
--- resources/views/content/users/edit.blade.php
+++ resources/views/content/users/edit.blade.php
@@ -3,27 +3,31 @@
 @php
     $content_header = [
         'page_title' => 'Пользователи',
-        'url_back' => session('users.index'),
-        'urlCreate' => ''
+        'url_back' => session('admin.users.index'),
+        'url_create' => ''
     ]
 @endphp
 
 @section('content')
 <section class="content">
-    <div class="box">
-        {!! Form::model(null, [
-            'method' => 'PATCH',
-            'url' => '#',
-            'files' => true
-        ]) !!}
-        <div class="box-header">
-            <i class="ion ion-clipboard"></i>
-            <h3 class="box-title">Редактирование пользователя <strong>{{ isset($user) ? $user->name : '' }}</strong></h3>
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+            <div class="box">
+                {!! Form::model(null, [
+                    'method' => 'PATCH',
+                    'url' => '#',
+                    'files' => true
+                ]) !!}
+                <div class="box-header">
+                    <i class="ion ion-clipboard"></i>
+                    <h3 class="box-title">Редактирование пользователя <strong>{{ isset($user) ? $user->name : '' }}</strong></h3>
+                </div>
+                <div class="box-body">
+                        @include('lte::content.users._form')
+                </div>
+                {!! Form::close() !!}
+            </div>
         </div>
-        <div class="box-body">
-                @include('lte::content.users._form')
-        </div>
-        {!! Form::close() !!}
     </div>
 </section>
 @stop
